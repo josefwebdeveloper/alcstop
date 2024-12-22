a0b3740600b3ac08b58c821afa5f57323dd4e525
Josef Shainskiy <josef2011@gmail.com>
2024-12-22 03:00:58 +0200

    upd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'alstop' title`, () => {
+  it(`should have the 'alcstop' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('alstop');
+    expect(app.title).toEqual('alcstop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, alstop');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, alcstop');
   });
 });
